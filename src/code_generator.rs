8d19b86d2c8b4d913869aa07544ea10a672119cb
Dylan Remus <dmr942@mail.usask.ca>
2023-05-17 19:14:32 -0600

    Refactor code generator to separate instruction formatting from instruction generation

--- src/code_generator.rs
+++ src/code_generator.rs
@@ -1,55 +1,79 @@
+use crate::code_generator::AsmToken::{InstructionNoArgs, InstructionOneArg, InstructionTwoArgs, FunctionStart};
 use crate::parser::{Expression, Function, Operation, Program, Statement};
-use std::fmt::format;
 
-pub fn generate_code(program: Program) -> String {
-    let Program(function): Program = program;
-    generate_asm_for_function(function)
+enum AsmToken {
+    FunctionStart(String),
+    InstructionNoArgs(String),
+    InstructionOneArg(String, String),
+    InstructionTwoArgs(String, String, String),
 }
 
-fn generate_asm_for_function(function: Function) -> String {
-    let Function(name, body) = function;
-    return format!(
-        concat! {
-        ".globl {}\n",
-        "{}:\n",
-        "{}\n"
-        },
-        name,
-        name,
-        generate_asm_for_statement(body)
-    );
-}
+struct AsmProgram (pub Vec<AsmToken>);
 
-fn generate_asm_for_statement(statement: Statement) -> String {
-    match statement {
-        Statement::Return(expression) => {
-            format!(
-                concat! {
-                "{}",
-                " ret"
-                },
-                generate_asm_for_expression(expression)
-            )
+impl From<&AsmToken> for String {
+    fn from(value: &AsmToken) -> Self {
+        match value {
+            FunctionStart(name) => format!(".globl {}\n{}:", name, name),
+            InstructionNoArgs(instruction) => format!("  {}", instruction),
+            InstructionOneArg(instruction, arg) => format!("  {} {}", instruction, arg),
+            InstructionTwoArgs(instruction, arg1, arg2) => format!("  {} {}, {}", instruction, arg1, arg2),
         }
     }
 }
 
-fn generate_asm_for_expression(expression: Expression) -> String {
+impl From<AsmProgram> for String {
+    fn from(value: AsmProgram) -> Self {
+        value.0.iter().map(|token| token.into()).collect::<Vec<String>>().join("\n")
+    }
+}
+
+pub fn generate_code(program: Program) -> String {
+    let Program(function): Program = program;
+    AsmProgram(generate_asm_for_function(function)).into()
+}
+
+fn generate_asm_for_function(function: Function) -> Vec<AsmToken> {
+    let Function(name, body) = function;
+
+    let mut asm = vec![FunctionStart(name)];
+    asm.extend(generate_asm_for_statement(body).into_iter());
+    asm
+}
+
+fn generate_asm_for_statement(statement: Statement) -> Vec<AsmToken> {
+    match statement {
+        Statement::Return(expression) => {
+            let mut x = vec![];
+            x.extend(generate_asm_for_expression(expression));
+            x.push(InstructionNoArgs("ret".into()));
+            x
+        }
+    }
+}
+
+fn generate_asm_for_expression(expression: Expression) -> Vec<AsmToken> {
     match expression {
-        Expression::Constant(int) => format!(" movl ${}, %eax\n", int.to_string()),
+        Expression::Constant(int) => vec![InstructionTwoArgs("movl".into(), int.to_string(), "%eax".into())],
         Expression::UnaryOperation(op, exp) => match op {
-            Operation::Negation => format!(
-                concat!("{}", " neg %eax\n",),
-                generate_asm_for_expression(*exp)
-            ),
-            Operation::BitwiseNot => format!(
-                concat!("{}", " not %eax\n",),
-                generate_asm_for_expression(*exp)
-            ),
-            Operation::LogicalNot => format!(
-                concat!("{}", " cmpl $0, %eax\n", " movl $0, %eax\n", " sete %al\n",),
-                generate_asm_for_expression(*exp)
-            ),
+            Operation::Negation => {
+                let mut x = generate_asm_for_expression(*exp);
+                x.extend(vec![InstructionOneArg("neg".into(), "%eax".into())]);
+                x
+            },
+            Operation::BitwiseNot => {
+                let mut x = generate_asm_for_expression(*exp);
+                x.extend(vec![InstructionOneArg("not".into(), "%eax".into())]);
+                x
+            },
+            Operation::LogicalNot => {
+                let mut x = generate_asm_for_expression(*exp);
+                x.extend(vec![
+                    InstructionTwoArgs("cmpl".into(), "$0".into(), "%eax".into()),
+                    InstructionTwoArgs("movl".into(), "$0".into(),"%eax".into()),
+                    InstructionOneArg("sete".into(), "%al".into()),
+                ]);
+                x
+            },
         },
     }
 }
